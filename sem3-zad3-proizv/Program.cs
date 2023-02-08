5fe5240b0e061bd9d04b3cc384ea7e648948c017
Algo <algor102@gmail.comm>
2023-02-08 23:35:55 +0500

    Произведение всех чисел от 1 до N

---
+++ sem3-zad3-proizv/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("-------------------");
+Console.WriteLine("Произведение всех чисел до N");
+int result = 1;
+for (int i = 1; i < N+1; i++)
+{
+  result = result * i;
+  Console.WriteLine($"{i}) {result}");
+}
+Console.WriteLine($"Произведенние {N} чисел равно {result}");
