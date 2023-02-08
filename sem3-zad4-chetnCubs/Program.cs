21b70e81ffa785765c81ddc3bca507afd0fc3da6
Algo <algor102@gmail.comm>
2023-02-09 00:04:54 +0500

    Ищем четные кубы чисел от 1 до N

---
+++ sem3-zad4-chetnCubs/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Четные кубы всех чисел до N");
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("-------------------");
+
+for (int i = 1; i < N+1; i++)
+{
+  double cube = Math.Pow(i, 3);
+  if (cube%2==0)
+  Console.WriteLine($"{i}) {cube}");
+}
