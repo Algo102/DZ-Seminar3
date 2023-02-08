847ed3736149d43ebdecbad1d58b4c624253b71f
Algo <algor102@gmail.comm>
2023-02-08 23:10:09 +0500

    Нахождение всех кубов до N

---
+++ sem3-zad1-cubs/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("-------------------");
+Console.WriteLine("Ищем все кубы");
+for (int i = 1; i < N+1; i++)
+{
+double cube = Math.Pow(i, 3);
+Console.WriteLine($"Куб числа {i} = {cube}");
+}
